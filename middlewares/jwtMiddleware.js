683804eac43f9150bc535dcc28cc80ee8f775c86
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-11-27 16:35:06 +0100

    Add JWT middleware for token validation and user authorization

---
+++ middlewares/jwtMiddleware.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const validateJWT = (req, res, next) => {
+  const token = req.headers.authorization.split(" ")[1];
+  if (!token)
+    return res.status(401).send("Access Denied - No token provided 😦");
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log(process.env.JWT_SECRET);
+    req.id = decoded.id;
+    next();
+  } catch (error) {
+    res.status(400).send("unauthorized accsess 😦");
+  }
+};
---
+++ routes/usersRoute.js
@@ -0,0 +1,22 @@
+import express from "express";
+import User from "../models/userModel.js";
+import {
+  createUser,
+  getAllUsers,
+  updatePasswordById,
+  updateUsernameById,
+  userLogin,
+} from "../controllers/userController.js";
+import { validateUser } from "../middlewares/validateUser.js";
+import { validateJWT } from "../middlewares/jwtMiddleware.js";
+const router = express.Router();
+
+router.get("/", validateJWT, getAllUsers);
+
+router.post("/create", createUser);
+
+router.post("/login", userLogin);
+router.patch("/update-username/:id",validateJWT, updateUsernameById);
+router.patch("/update-password/:id",validateJWT, updatePasswordById);
+
+export default router;
