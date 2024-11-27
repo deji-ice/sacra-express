631e937f89094ffaf423ffd322598f51c41304ae
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-11-27 16:35:01 +0100

    Add userController with createUser, getAllUsers, userLogin, updateUsernameById, and updatePasswordById functions

---
+++ controllers/userController.js
@@ -0,0 +1,108 @@
+import User from "../models/userModel.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const createUser = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).send("User already exists");
+    }
+
+    const user = new User(req.body);
+    const saltRounds = 10;
+
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    user.password = hashedPassword;
+    await user.save();
+    res.status(201).send("User created");
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+};
+
+export const getAllUsers = async (req, res) => {
+  try {
+    const allUsers = await User.find({}, { password: 0 });
+    res.status(200).json(allUsers);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+};
+
+export const userLogin = async (req, res) => {
+  try {
+    // throw new Error("Simulating a server-side error");
+    const { username, password } = req.body;
+    if (!username || !password) {
+      return res.status(401).send("Please provide username and password");
+    }
+
+    const user = await User.findOne({ username });
+
+    if (!user) return res.status(401).send("username does not exist");
+    const isCorrectPassword = await bcrypt.compare(password, user.password);
+
+    if (!isCorrectPassword) {
+      return res.status(401).send("Invalid password");
+    }
+    const token = jwt.sign({ id: user._id },process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    }); // expires in 1 hour
+
+    console.log(process.env.JWT_SECRET);
+    res.status(200).json({message: "Login successful", token});
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+export const updateUsernameById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { newUsername } = req.body;
+    const user = await User.findByIdAndUpdate(id, { username: newUsername });
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+    res.status(200).send("Username updated successfully");
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+export const updatePasswordById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { newPassword, oldPassword } = req.body;
+    if (!newPassword || !oldPassword)
+      return res.status(400).send("Please provide old and new password");
+
+    if (newPassword === oldPassword)
+      return res
+        .status(400)
+        .send("New password cannot be the same as old password");
+
+    const user = await User.findBy(id);
+    if (!user) return res.status(404).send("User not found");
+
+    const isOldPasswordCorrect = await bcrypt.compare(
+      oldPassword,
+      user.password
+    );
+    if (!isOldPasswordCorrect) {
+      return res.status(401).send("Old password is incorrect");
+    }
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(newPassword, saltRounds);
+    user.password = hashedPassword;
+    await user.save();
+    res.status(200).send("Password updated successfully");
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
---
+++ db/conn.js
@@ -0,0 +1,42 @@
+// import { MongoClient, ServerApiVersion } from "mongodb";
+// import mongoose from "mongoose";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+
+const dbConnection = async () => {
+  try {
+    await mongoose.connect(process.env.ATLAS_URI);
+    console.log("Database connected successfully");
+    return mongoose.connection;
+  } catch (error) {
+    console.log("Error while connecting to database", error);
+  }
+};
+
+
+export default dbConnection;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(connectionString, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   },
+// });
+
+// let conn;
+
+// try {
+//   conn = await client.connect();
+//   console.log("mongo connected")
+// } catch (e) {
+//   console.error(e);
+// }
+
+// let db = conn.db("sample_mflix");
+
+// export default db;
